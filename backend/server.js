4f8070c9d2799f42fc0f114030c8b32d31518c9e
TanishkSharma13 <tanishksh1311@gmail.com>
2026-05-10 18:59:23 +0530

    Fixed production CORS

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
+
 const authRoutes = require("./routes/authRoutes");
 const projectRoutes = require("./routes/projectRoutes");
 const taskRoutes = require("./routes/taskRoutes");
@@ -12,20 +13,34 @@ dotenv.config();
 
 const app = express();
 
+// Connect Database
 connectDB();
 
-app.use(cors());
+// Middlewares
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://task-manager-eight-beryl.vercel.app",
+    ],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 
+// Test Route
 app.get("/", (req, res) => {
   res.send("API Running");
 });
 
+// Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
